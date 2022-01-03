2d189704127ac19adc521212927364d754ebe17f
Duncan Nielsen <duncann008@gmail.com>
2022-01-03 15:59:31 -0600

    got that pesky ternary solved

--- src/components/MenuItemDetails/MenuItemDetails.jsx
+++ src/components/MenuItemDetails/MenuItemDetails.jsx
@@ -15,25 +15,37 @@ function MenuItemDetails() {
 
   const menuItem = useSelector(store => store.menuItemReducer)
 
+  const ingredients = (ingredient) => {
+    switch (ingredient) {
+      case 'None':
+        return false;
+      default:
+        return true;
+    }
+  };
+
+  console.log(ingredients(menuItem.Sauce));
+
   return (
 
     <div>
         <h1>{menuItem.item}</h1>
         <img src={menuItem.image_url} height="200" />
+        <p>Included Ingredients:</p>
         <ul>
-            <li>Shell: {menuItem.Shell}</li>
-            <li>Meat: {menuItem.Meat}</li>
-            <li>Cheese: {menuItem.Cheese}</li>
-            <li>Beans: {menuItem.Beans}</li>
-            <li>Rice: {menuItem.Rice}</li>
-            <li>Lettuce: {menuItem.Lettuce}</li>
-            <li>Salsa: {menuItem.Salsa}</li>
-            <li>Sour Cream: {menuItem.SourCream}</li>
-            <li>Pico de Gallo: {menuItem.PicodeGallo}</li>
-            <li>Cilantro: {menuItem.Cilantro}</li>
-            <li>Diced Onions: {menuItem.DicedOnions}</li>
-            <li>Sauce: {menuItem.Sauce}</li>
-            <li>Lime: {menuItem.Lime}</li>
+            {ingredients(menuItem.Shell) ? <li>Shell: {menuItem.Shell}</li> : ''}
+            {ingredients(menuItem.Meat) ? <li>Meat: {menuItem.Meat}</li> : ''}
+            {ingredients(menuItem.Cheese) ? <li>Cheese: {menuItem.Cheese}</li> : ''}
+            {ingredients(menuItem.Beans) ? <li>Beans: {menuItem.Beans}</li> : ''}
+            {ingredients(menuItem.Rice) ? <li>Rice: {menuItem.Rice}</li> : ''}
+            {ingredients(menuItem.Lettuce) ? <li>Lettuce: {menuItem.Lettuce}</li> : ''}
+            {ingredients(menuItem.Salsa) ? <li>Salsa: {menuItem.Salsa}</li> : ''}
+            {ingredients(menuItem.SourCream) ? <li>Sour Cream: {menuItem.SourCream}</li> : ''}
+            {ingredients(menuItem.PicodeGallo) ? <li>Pico de Gallo: {menuItem.PicodeGallo}</li> : ''}
+            {ingredients(menuItem.Cilantro) ? <li>Cilantro: {menuItem.Cilantro}</li> : ''}
+            {ingredients(menuItem.DicedOnions) ? <li>Diced Onions: {menuItem.DicedOnions}</li> : ''}
+            {ingredients(menuItem.Sauce) ? <li>Sauce: {menuItem.Sauce}</li> : ''}
+            {ingredients(menuItem.Lime) ? <li>Lime: {menuItem.Lime}</li> : ''}
         </ul>
     </div>
 
