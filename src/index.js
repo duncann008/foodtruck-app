3038a0384541fbd56211b40c79c5ce0f763be368
Duncan Nielsen <duncann008@gmail.com>
2022-01-04 12:45:50 -0600

    redux persist working for my cart reducer!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
-import store from './redux/store';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor, sagaMiddleware } from './redux/store.js';
 import App from './components/App/App';
+import rootSaga from './redux/sagas/_root.saga';
+
+sagaMiddleware.run(rootSaga); 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-    
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('react-root'),
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './reducers/_root.reducer'; // imports ./redux/reducers/index.js
 import rootSaga from './sagas/_root.saga'; // imports ./redux/sagas/index.js
 
 const sagaMiddleware = createSagaMiddleware();
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cartReducer']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // this line creates an array of all of redux middleware you want to use
 // we don't want a whole ton of console logs in our production code
@@ -14,16 +24,23 @@ const middlewareList = process.env.NODE_ENV === 'development' ?
   [sagaMiddleware, logger] :
   [sagaMiddleware];
 
-const store = createStore(
-  // tells the saga middleware to use the rootReducer
-  // rootSaga contains all of our other reducers
-  rootReducer,
-  // adds all middleware to our project including saga and logger
-  applyMiddleware(...middlewareList),
-);
+
 
 // tells the saga middleware to use the rootSaga
 // rootSaga contains all of our other sagas
-sagaMiddleware.run(rootSaga);
 
-export default store;
+
+const store = createStore(
+  // tells the saga middleware to use the rootReducer
+  // rootSaga contains all of our other reducers
+  persistedReducer,
+  // rootReducer, 
+  // adds all middleware to our project including saga and logger
+  applyMiddleware(...middlewareList),
+);
+const persistor = persistStore(store);
+sagaMiddleware.run(rootSaga); 
+
+export {store, persistor, sagaMiddleware};
+
+
